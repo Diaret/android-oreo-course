a162c8a61774ad0ef69e6240624611812979751d
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-01-14 01:40:40 +0300

    S03L36. java classes

---
+++ java deep dive/src/jdd/Classes.java
@@ -0,0 +1,25 @@
+package jdd;
+
+public class Classes {
+
+	public static void main(String[] args) {
+		
+		class User {
+			int score;
+			public boolean hasWon() {
+				if (score >= 100) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+		
+		User bob = new User();
+		bob.score = 200;
+		
+		System.out.println(bob.score);
+		System.out.println(bob.hasWon());
+	}
+
+}
