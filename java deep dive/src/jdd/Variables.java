8db0c0c755f5e02bbdee0d36057b910159770eab
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-01-13 23:25:48 +0300

    S03L27. java variables

---
+++ java deep dive/src/jdd/Variables.java
@@ -0,0 +1,31 @@
+package jdd;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int age = 34;
+		int favNumber = 777;
+		System.out.println(favNumber*100 + age);
+		
+		double favNumber2 = 3.5;
+		System.out.println(favNumber2);
+		
+		boolean amIMale = true;
+		System.out.println("Am I a male?: "+ amIMale);
+		
+		String myName = "Dmitrij";
+		System.out.println("My name is "+ myName + ".");
+		
+		System.out.println("My name is "+ myName + ". I am " + age + " years old.");
+		
+		double a = 6.7;
+		double b = 3.2;
+		System.out.println(a * b);
+		System.out.println(a + b - 7.3);
+		System.out.println(a - b);
+		
+		String myString = "djfjrgGTF klsjfd ytYF *874895y dsgf";
+		System.out.println(myString.length());
+	}
+
+}
