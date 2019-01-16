993988bef9996b1c76bdbfb0516b703d744569e7
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-01-17 01:20:29 +0300

    S04L54. Audio Seekbars

--- SoundDemo/app/src/main/java/com/example/sounddemo/MainActivity.java
+++ SoundDemo/app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.sounddemo;
 
 import android.content.res.AssetManager;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     public void playSound(View view){
 //        Button playButton = findViewById(R.id.playButton);
@@ -25,10 +34,78 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.hoovering);
+        SeekBar volumeControl = findViewById(R.id.seekBar);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("volume", Integer.toString(progress));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar scrabControl = findViewById(R.id.scrabSeekBar);
+
+        scrabControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("scrab", Integer.toString(progress));
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                double d;
+                double d1 = mediaPlayer.getCurrentPosition();
+                double d2 = mediaPlayer.getDuration();
+                d = round(d1/d2 * 100, 2);
+                Log.i("cPos", Integer.toString(mediaPlayer.getCurrentPosition()));
+                Log.i("Duration", Integer.toString(mediaPlayer.getDuration()));
+                Log.i("cP", Double.toString(d));
+                Double D = d;
+                scrabControl.setProgress(D.intValue());
+            }
+        },0, 300);
 
     }
 
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
 
 
 }
