3e7a9d1f233e6356ec7a9d483bcda34540713ef5
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-01-23 00:44:52 +0300

    S05L79 Split and Regex v02

--- java deep dive/src/jdd/SplitString.java
+++ java deep dive/src/jdd/SplitString.java
@@ -1,5 +1,7 @@
 package jdd;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class SplitString {
 
 	public static void main(String[] args) {
@@ -13,6 +15,13 @@ public class SplitString {
 		
 		String riverPart = river.substring(2, 5);
 		System.out.println(riverPart);
+		
+		Pattern p = Pattern.compile("Mi(.*?)pi");
+		Matcher m = p.matcher(river);
+		
+		while (m.find()) {
+			System.out.println(m.group(1));
+		}
 		
 	}
 
