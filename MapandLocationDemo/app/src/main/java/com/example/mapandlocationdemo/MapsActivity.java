7599727b30d413319569c0ca92783cd255822648
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-01-27 17:24:12 +0300

    S06L97 Location Information

--- MapandLocationDemo/app/src/main/java/com/example/mapandlocationdemo/MapsActivity.java
+++ MapandLocationDemo/app/src/main/java/com/example/mapandlocationdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.mapandlocationdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +13,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +21,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import javax.security.auth.login.LoginException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     LocationManager locationManager;
@@ -73,10 +82,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onLocationChanged(Location location) {
                 //Log.i("LocationChanged", String.valueOf(location.getLatitude()));
-                LatLng currPos = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.clear();
+                LatLng currPos = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(currPos).title("You are here!"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currPos,latLngZoom));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
+                    if (listAddresses != null && listAddresses.size()>0){
+                        String address = "";
+                        if (listAddresses.get(0).getThoroughfare() != null){
+                            address += listAddresses.get(0).getThoroughfare() + " ";
+                        }
+
+                        if (listAddresses.get(0).getLocality() != null){
+                            address += listAddresses.get(0).getLocality() + " ";
+                        }
+
+                        if (listAddresses.get(0).getPostalCode() != null){
+                            address += listAddresses.get(0).getPostalCode() + " ";
+                        }
+
+                        if (listAddresses.get(0).getAdminArea() != null){
+                            address += listAddresses.get(0).getAdminArea() + " ";
+                        }
+
+                        Log.i("PlaceInfo", address);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -100,8 +136,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } else{
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
             Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            LatLng currPos = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
             mMap.clear();
+            LatLng currPos = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
             mMap.addMarker(new MarkerOptions().position(currPos).title("You are here!"));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currPos,latLngZoom));
         }
