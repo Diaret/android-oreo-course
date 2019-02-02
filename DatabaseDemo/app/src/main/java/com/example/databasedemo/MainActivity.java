8c75b315f39268004baa6a5b6ba2a0c08015c424
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-02-03 00:27:44 +0300

    S07L114 Create SqlLite DB: users table

--- DatabaseDemo/app/src/main/java/com/example/databasedemo/MainActivity.java
+++ DatabaseDemo/app/src/main/java/com/example/databasedemo/MainActivity.java
@@ -13,19 +13,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase myDB = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
-        myDB.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
-        //myDB.execSQL("INSERT INTO events (name, year) VALUES ('Millenium', 2000)");
-        //myDB.execSQL("INSERT INTO events (name, year) VALUES ('Nick started teaching', 2014)");
+        try {
+            SQLiteDatabase myDB = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+            myDB.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+            myDB.execSQL("DELETE FROM users");
+            myDB.execSQL("INSERT INTO users (name, age) VALUES ('Nick', 29)");
+            myDB.execSQL("INSERT INTO users (name, age) VALUES ('Marta', 33)");
+            myDB.execSQL("INSERT INTO users (name, age) VALUES ('Daniel', 12)");
+            myDB.execSQL("INSERT INTO users (name, age) VALUES ('Tomas', 43)");
+            myDB.execSQL("INSERT INTO users (name, age) VALUES ('Dave', 35)");
 
-        Cursor cursor = myDB.rawQuery("SELECT * FROM events", null);
-        int nameIndex = cursor.getColumnIndex("name");
-        int yearIndex = cursor.getColumnIndex("year");
-        cursor.moveToFirst();
-        while (cursor!=null){
-            Log.i("name", cursor.getString(nameIndex));
-            Log.i("year", String.valueOf(cursor.getInt(yearIndex)));
-            cursor.moveToNext();
+            Cursor cursor = myDB.rawQuery("SELECT * FROM users WHERE age >= 18 AND name LIKE '%a%' LIMIT 2", null);
+            int nameIndex = cursor.getColumnIndex("name");
+            int ageIndex = cursor.getColumnIndex("age");
+            cursor.moveToFirst();
+            while (cursor != null) {
+                Log.i("name", cursor.getString(nameIndex));
+                Log.i("age", String.valueOf(cursor.getInt(ageIndex)));
+                cursor.moveToNext();
+            }
+        } catch (Exception e) {
+//            e.printStackTrace();
         }
 
 
