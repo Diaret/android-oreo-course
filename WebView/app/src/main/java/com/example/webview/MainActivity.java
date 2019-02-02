a1c27a85bd09cb9f498a32e4c53662cdb92a8ad4
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-02-03 00:36:20 +0300

    S07L118 WebView

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.rbc.ru");
+        webView.loadData("<html><body><h1>Hello world!</h1><p>This is my cool website</p></body></html>", "text/html", "UTF-8");
+
+    }
+}
