b2b6163ae83448bd68264411de389d6343ccb4f4
Dmitrij Lebedev <mr.diaret@ya.ru>
2019-02-03 19:39:28 +0300

    Room Word Sample: Adding DAO

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/Word.java
@@ -0,0 +1,23 @@
+package com.example.roomwordsample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey //(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public String getmWord() {
+        return this.mWord;
+    }
+
+    public Word(@NonNull String mWord) {
+        this.mWord = mWord;
+    }
+}
